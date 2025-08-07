f80a80f046e852cd62ce8c454d1e902d128743cf
gmanka <gmankab@gmail.com>
2025-08-07 07:46:56 +0500

    [r] refactor artifacts building

--- scripts/artifacts_mipad5.sh
+++ scripts/artifacts_mipad5.sh
@@ -3,25 +3,22 @@
 set -uexo pipefail
 
 which 7z
+which git
+which curl
+which python
 
-mkdir artifacts/images
-mv artifacts/root.raw artifacts/images
-mv artifacts/boot.raw artifacts/images
-mv artifacts/esp.raw artifacts/images
-
-curl -L https://github.com/map220v/android_device_xiaomi_nabu/releases/download/nabu_twrp-12.1_20230531/boot.img -o artifacts/images/twrp.img
-curl -L https://github.com/ArKT-7/automated-nabu-lineage-installer/releases/download/lineage-22.1-20250207-UNOFFICIAL-nabu/dtbo.img -o artifacts/images/dtbo.img
-curl -L https://github.com/gmankab/sgdisk/releases/download/v1.0.10/sgdisk -o artifacts/images/sgdisk
-curl -L https://github.com/gmankab/parted/releases/download/v3.6/parted -o artifacts/images/parted
+curl -L https://github.com/map220v/android_device_xiaomi_nabu/releases/download/nabu_twrp-12.1_20230531/boot.img -o images/twrp.img
+curl -L https://github.com/ArKT-7/automated-nabu-lineage-installer/releases/download/lineage-22.1-20250207-UNOFFICIAL-nabu/dtbo.img -o images/dtbo.img
+curl -L https://github.com/gmankab/sgdisk/releases/download/v1.0.10/sgdisk -o images/sgdisk
+curl -L https://github.com/gmankab/parted/releases/download/v3.6/parted -o images/parted
 
 curl -L https://gitlab.com/sm8150-mainline/u-boot/-/jobs/9720400108/artifacts/download -o uboot.zip
 7z x uboot.zip -o./uboot
-cp uboot/.output/u-boot.img artifacts/images/uboot.img
+cp uboot/.output/u-boot.img images/uboot.img
 
 git clone --depth=1 https://android.googlesource.com/platform/external/avb
-python avb/avbtool.py make_vbmeta_image --flags 2 --padding_size 4096 --output artifacts/images/vbmeta_disabled.img
+python avb/avbtool.py make_vbmeta_image --flags 2 --padding_size 4096 --output images/vbmeta_disabled.img
 
-install -Dm 0755 scripts/flash_mipad5.sh artifacts/flash_mipad5.sh
+install -Dm 0755 scripts/flash_mipad5.sh flash_mipad5.sh
 
-cd artifacts
-7z -mx=9 a "../pocketblue-$IMAGE_NAME-$IMAGE_TAG.7z" *
+7z -mx=9 a "pocketblue-$IMAGE_NAME-$IMAGE_TAG.7z" flash_mipad5.sh images
--- scripts/artifacts_oneplus6.sh
+++ scripts/artifacts_oneplus6.sh
@@ -1,2 +1,17 @@
-cd artifacts
-7z -mx=9 a "../pocketblue-$IMAGE_NAME-$IMAGE_TAG.7z" *
+#!/usr/bin/env bash
+
+set -uexo pipefail
+
+which 7z
+which curl
+
+curl -L https://github.com/fedora-remix-mobility/u-boot/releases/download/fedora-mobility-v0.0.1/uboot-sdm845-oneplus-enchilada.img -o images/uboot-enchilada.img
+curl -L https://github.com/fedora-remix-mobility/u-boot/releases/download/fedora-mobility-v0.0.1/uboot-sdm845-oneplus-fajita.img -o images/uboot-fajita.img
+
+install -Dm 0755 scripts/flash_oneplus6.sh.in flash_oneplus6_enchilada.sh
+install -Dm 0755 scripts/flash_oneplus6.sh.in flash_oneplus6t_fajita.sh
+
+sed -i 's/@device@/enchilada/g' flash_oneplus6_enchilada.sh
+sed -i 's/@device@/fajita/g'    flash_oneplus6t_fajita.sh
+
+7z -mx=9 a "pocketblue-$IMAGE_NAME-$IMAGE_TAG.7z" flash_oneplus6*.sh images
