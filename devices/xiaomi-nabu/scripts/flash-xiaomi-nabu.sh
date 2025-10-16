2d5a94601e895df45cc740462d52a61ad53c12fe
gmanka <gmankab@gmail.com>
2025-10-16 22:09:02 +0500

    [u] flash to rawdump and cust partitions

--- devices/xiaomi-nabu/scripts/flash-xiaomi-nabu.sh
+++ devices/xiaomi-nabu/scripts/flash-xiaomi-nabu.sh
@@ -10,8 +10,8 @@ fastboot getvar product 2>&1 | grep nabu
 fastboot erase dtbo_ab
 fastboot flash vbmeta_ab images/vbmeta-disabled.img
 fastboot flash   boot_ab images/uboot.img
-fastboot flash      cust images/fedora_esp.raw
-fastboot flash     super images/fedora_boot.raw
+fastboot flash   rawdump images/fedora_esp.raw
+fastboot flash      cust images/fedora_boot.raw
 fastboot flash  userdata images/fedora_rootfs.raw
 
 echo 'done flashing, rebooting now'
--- devices/xiaomi-pipa/scripts/flash-xiaomi-pipa.cmd
+++ devices/xiaomi-pipa/scripts/flash-xiaomi-pipa.cmd
@@ -9,8 +9,8 @@ fastboot getvar product 2>&1 | findstr /i pipa || (pause & exit)
 fastboot erase dtbo_ab || (pause & exit)
 fastboot flash vbmeta_ab images/vbmeta-disabled.img || (pause & exit)
 fastboot flash   boot_ab images/kxboot.img || (pause & exit)
-fastboot flash      cust images/fedora_esp.raw || (pause & exit)
-fastboot flash     super images/fedora_boot.raw || (pause & exit)
+fastboot flash   rawdump images/fedora_esp.raw || (pause & exit)
+fastboot flash      cust images/fedora_boot.raw || (pause & exit)
 fastboot flash  userdata images/fedora_rootfs.raw || (pause & exit)
 
 echo 'done flashing, rebooting device now'
