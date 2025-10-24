973bc33c1ef7242e33ba0db94de8fb06e8514592
onesaladleaf <180638599+onesaladleaf@users.noreply.github.com>
2025-10-24 21:11:04 +0300

    Update flash scripts location in artifacts.sh

--- devices/xiaomi-nabu/build-aux/artifacts.sh
+++ devices/xiaomi-nabu/build-aux/artifacts.sh
@@ -13,5 +13,5 @@ cp uboot/.output/u-boot.img $OUT_PATH/images/uboot.img
 git clone --depth=1 https://android.googlesource.com/platform/external/avb
 python avb/avbtool.py make_vbmeta_image --flags 2 --padding_size 4096 --output $OUT_PATH/images/vbmeta-disabled.img
 
-install -Dm 0755 $DEVICE_PATH/scripts/flash-xiaomi-nabu.sh $OUT_PATH/flash-xiaomi-nabu.sh
-install -Dm 0755 $DEVICE_PATH/scripts/flash-xiaomi-nabu.cmd $OUT_PATH/flash-xiaomi-nabu.cmd
+install -Dm 0755 $DEVICE_PATH/flash-scripts/flash-xiaomi-nabu.sh $OUT_PATH/flash-xiaomi-nabu.sh
+install -Dm 0755 $DEVICE_PATH/flash-scripts/flash-xiaomi-nabu.cmd $OUT_PATH/flash-xiaomi-nabu.cmd
--- devices/xiaomi-pipa/build-aux/artifacts.sh
+++ devices/xiaomi-pipa/build-aux/artifacts.sh
@@ -5,5 +5,5 @@ set -uexo pipefail
 git clone --depth=1 https://android.googlesource.com/platform/external/avb
 python avb/avbtool.py make_vbmeta_image --flags 2 --padding_size 4096 --output $OUT_PATH/images/vbmeta-disabled.img
 
-install -Dm 0755 $DEVICE_PATH/scripts/flash-xiaomi-pipa.sh $OUT_PATH/flash-xiaomi-pipa.sh
-install -Dm 0755 $DEVICE_PATH/scripts/flash-xiaomi-pipa.cmd $OUT_PATH/flash-xiaomi-pipa.cmd
+install -Dm 0755 $DEVICE_PATH/flash-scripts/flash-xiaomi-pipa.sh $OUT_PATH/flash-xiaomi-pipa.sh
+install -Dm 0755 $DEVICE_PATH/flash-scripts/flash-xiaomi-pipa.cmd $OUT_PATH/flash-xiaomi-pipa.cmd
