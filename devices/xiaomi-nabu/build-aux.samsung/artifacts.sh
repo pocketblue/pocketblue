5a3d2898ea9f999fae12266fa9c1f1a3511d04b7
onesaladleaf <180638599+onesaladleaf@users.noreply.github.com>
2026-01-23 06:40:02 +0300

    xiaomi-nabu: add flash scripts for samsung ufs images

--- devices/xiaomi-nabu/build-aux.samsung/artifacts.sh
+++ devices/xiaomi-nabu/build-aux.samsung/artifacts.sh
@@ -10,9 +10,12 @@ mv $OUT_PATH/aloha.zip ./
 7z x aloha.zip -o./aloha
 cp aloha/xiaomi-nabu/xiaomi-nabu_NOSB.img $OUT_PATH/images/aloha.img
 
+mkdir -p $OUT_PATH/bin
+mv $OUT_PATH/sgdisk $OUT_PATH/bin/sgdisk
+mv $OUT_PATH/parted $OUT_PATH/bin/parted
+
 git clone --depth=1 https://android.googlesource.com/platform/external/avb
 python avb/avbtool.py make_vbmeta_image --flags 2 --padding_size 4096 --output $OUT_PATH/images/vbmeta-disabled.img
 
-# todo
-# install -Dm 0755 $DEVICE_PATH/flash-scripts/flash-xiaomi-nabu.sh $OUT_PATH/flash-xiaomi-nabu.sh
-# install -Dm 0755 $DEVICE_PATH/flash-scripts/flash-xiaomi-nabu.cmd $OUT_PATH/flash-xiaomi-nabu.cmd
+install -Dm 0755 $DEVICE_PATH/flash-scripts/flash-xiaomi-nabu-samsung.sh $OUT_PATH/flash-xiaomi-nabu.sh
+install -Dm 0755 $DEVICE_PATH/flash-scripts/flash-xiaomi-nabu-samsung.cmd $OUT_PATH/flash-xiaomi-nabu.cmd
