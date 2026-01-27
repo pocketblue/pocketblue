c51684a7fc1013d80594aa3b7f0999c495b1f56f
ergolyam <grisha765@tuta.io>
2026-01-27 19:45:53 +0300

    [f] fix unpack

--- devices/xunlong-orangepi3-lts/build-aux/artifacts.sh
+++ devices/xunlong-orangepi3-lts/build-aux/artifacts.sh
@@ -8,6 +8,9 @@ if [ -f "$uboot_deb" ]; then
     7z x -o"$tmp_dir" "$uboot_deb"
     data_tar="$(find "$tmp_dir" -maxdepth 1 -name 'data.tar.*' -print -quit)"
     7z x -o"$tmp_dir/data" "$data_tar"
+    if [ -f "$tmp_dir/data/data.tar" ]; then
+        7z x -o"$tmp_dir/data" "$tmp_dir/data/data.tar"
+    fi
     uboot_bin="$(find "$tmp_dir/data" -name 'u-boot-sunxi-with-spl.bin' -print -quit)"
     if [ -z "$uboot_bin" ]; then
         echo "u-boot-sunxi-with-spl.bin not found in $uboot_deb" >&2
