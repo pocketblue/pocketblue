d3bc2fdf8f5f56da79ed38639c5e2a19d9e5fd91
ergolyam <grisha765@tuta.io>
2026-02-10 22:35:45 +0300

    [f] dnf in podman

--- devices/xunlong-orangepi3-lts/build-aux/artifacts.sh
+++ devices/xunlong-orangepi3-lts/build-aux/artifacts.sh
@@ -4,10 +4,13 @@ set -uexo pipefail
 
 uboot_bin="$OUT_PATH/u-boot-sunxi-with-spl.bin"
 
-dnf -y install bsdtar 'dnf5-command(copr)'
-dnf -y copr enable ergolyam/rpms-orangepi3-lts
-dnf download --repo=copr:copr.fedorainfracloud.org:ergolyam:rpms-orangepi3-lts --destdir=$OUT_PATH u-boot
-bsdtar -xOf $OUT_PATH/u-boot-*.rpm '*/u-boot-sunxi-with-spl.bin' > $uboot_bin
+podman run --rm quay.io/fedora/fedora-minimal:latest \
+    bash -uexo pipefail -c "exec 3>&1 1>&2
+        dnf -y install bsdtar 'dnf5-command(copr)'
+        dnf -y copr enable ergolyam/rpms-orangepi3-lts
+        dnf download --repo=copr:copr.fedorainfracloud.org:ergolyam:rpms-orangepi3-lts --destdir=/tmp u-boot
+        1>&3 bsdtar -xOf /tmp/u-boot-*.aarch64.rpm '*/u-boot-sunxi-with-spl.bin'
+    " > $uboot_bin
 
 if [ -f "$uboot_bin" ]; then
     if [ -f "$OUT_PATH/disk.raw" ]; then
