9b8e40433a8a8472fb5543c88ba8a07f2c3571fc
ergolyam <grisha765@tuta.io>
2026-02-14 17:50:39 +0300

    [r] temp file

--- devices/xunlong-orangepi3-lts/build-aux/artifacts.sh
+++ devices/xunlong-orangepi3-lts/build-aux/artifacts.sh
@@ -4,7 +4,7 @@ set -uexo pipefail
 
 SCRIPT_DIR="$(dirname "$0")"
 
-uboot_bin="$OUT_PATH/u-boot-sunxi-with-spl.bin"
+uboot_bin="/tmp/u-boot-sunxi-with-spl.bin"
 disk_raw="$OUT_PATH/disk.raw"
 
 podman run --rm -i quay.io/fedora/fedora-minimal:latest \
@@ -15,4 +15,3 @@ start_lba=16
 ss=512
 start_bytes=$(( start_lba * ss ))
 dd if="$uboot_bin" of="$disk_raw" oflag=seek_bytes seek="$start_bytes" conv=notrunc
-rm "$uboot_bin"
