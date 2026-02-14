c550c96c8a86ba83bd228824f2135487e9ff9c55
ergolyam <grisha765@tuta.io>
2026-02-14 17:32:16 +0300

    [r] separation

--- devices/xunlong-orangepi3-lts/build-aux/artifacts.sh
+++ devices/xunlong-orangepi3-lts/build-aux/artifacts.sh
@@ -3,18 +3,14 @@
 set -uexo pipefail
 
 uboot_bin="$OUT_PATH/u-boot-sunxi-with-spl.bin"
+disk_raw="$OUT_PATH/disk.raw"
 
-podman run --rm quay.io/fedora/fedora-minimal:latest \
-    bash -uexo pipefail -c "exec 3>&1 1>&2
-        dnf -y install bsdtar 'dnf5-command(copr)'
-        dnf -y copr enable ergolyam/rpms-orangepi3-lts
-        dnf download --repo=copr:copr.fedorainfracloud.org:ergolyam:rpms-orangepi3-lts --destdir=/tmp u-boot
-        1>&3 bsdtar -xOf /tmp/u-boot-*.aarch64.rpm '*/u-boot-sunxi-with-spl.bin'
-    " > $uboot_bin
+podman run --rm -i quay.io/fedora/fedora-minimal:latest \
+    bash -s < "$OUT_PATH/extract-uboot.sh" > "$uboot_bin"
 
-sgdisk --resize-table 56 "$OUT_PATH/disk.raw"
+sgdisk --resize-table 56 "$disk_raw"
 start_lba=16
 ss=512
 start_bytes=$(( start_lba * ss ))
-dd if="$uboot_bin" of="$OUT_PATH/disk.raw" oflag=seek_bytes seek="$start_bytes" conv=notrunc
-rm $uboot_bin
+dd if="$uboot_bin" of="$disk_raw" oflag=seek_bytes seek="$start_bytes" conv=notrunc
+rm "$uboot_bin"
